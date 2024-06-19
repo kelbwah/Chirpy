e67bf4253c8ef2f147fdc0d099993a826bb3b74d
kelbwah <kbebops@gmail.com>
2024-06-18 20:00:58 -0700

    Added JWT support

--- main.go
+++ main.go
@@ -1,20 +1,75 @@
 package main
 
 import (
-    "net/http"
-    "log"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/kelbwah/Chirpy/internal/database"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-    const filePathRoot = "."
-    const port = "8080"
+type apiConfig struct {
+	fileserverHits int
+	DB             *database.DB
+	jwtSecret      string
+}
 
-    mux := http.NewServeMux() 
-    mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
-    server := &http.Server{
-        Addr: ":" + port,
-        Handler: mux,
-    }
-    log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
-    log.Fatal(server.ListenAndServe())
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	godotenv.Load(".env")
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("JWT_SECRET environment variable is not set")
+	}
+
+	db, err := database.NewDB("database.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbg := flag.Bool("debug", false, "Enable debug mode")
+	flag.Parse()
+	if dbg != nil && *dbg {
+		err := db.ResetDB()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+		DB:             db,
+		jwtSecret:      jwtSecret,
+	}
+
+	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/*", fsHandler)
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
+
+	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
+
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
+
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
+	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
